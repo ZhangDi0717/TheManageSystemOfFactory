4fb7aa7c331c86cb0ed8c0f8e758a0d9c8cd9750
ZhangDi0717 <2159995470@qq.com>
2021-06-10 14:18:59 +0800

    管理员的申请单管理和员工管理初步完成

--- 01-web/src/main/java/com/zhang/dao/EmployeeInformationImpl.java
+++ 01-web/src/main/java/com/zhang/dao/EmployeeInformationImpl.java
@@ -4,8 +4,11 @@ import com.zhang.entity.Employee;
 import com.zhang.entity.EmployeeInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface EmployeeInformationImpl extends JpaRepository<EmployeeInformation,Long> {
     EmployeeInformation findByEmployee(Employee employee);
 
+    List<EmployeeInformation> findBySex(Integer sex);
 }
